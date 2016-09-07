70fab1a99e014d81ace5265924e774bb4796ba47
waqaskhan <waqaskhan132@gmail.com>
2016-09-07 22:10:47 +0500

    admin site changes

---
+++ App_Code/adminjobseekers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for adminjobseekers
+/// </summary>
+public class adminjobseekers
+{
+    public adminjobseekers()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public static IQueryable<jobSeeker> getjobseekers()
+    {
+        DataClassesDataContext db = new DataClassesDataContext();
+        IQueryable<jobSeeker> data = from x in db.jobSeekers
+                                     select x;
+        return data;
+    }
+}
